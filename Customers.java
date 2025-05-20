cbb0efbf5f51f6fd30133dcad8b30d4fa251f9d4
WiLLr590 <wramirez590@outlook.com>
2025-05-20 14:55:11 -0500

    Agregar gestion de Clientes

---
+++ Customers.java
@@ -0,0 +1,6 @@
+public class Customers{
+    public static void main(String[] args){
+        System.out.println("Menu de Clientes");
+        System.out.println("Obteniendo lista de Clientes...");
+    }
+}
